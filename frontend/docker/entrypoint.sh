5f1fb7d4ad37c1c4da6a1d1ae238b824ba08b940
akablockchain2 <akablockchain2@gmail.com>
2025-02-23 06:27:54 +0000

    all variables in entrypoint

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -5,7 +5,17 @@ echo "Injecting environment variables into HTML files..."
 # Перебор всех HTML файлов и подстановка переменных
 for file in $(find /usr/share/nginx/html -type f -name "*.html"); do
   echo "Processing $file"
-  sed -i "s|%NX_DATA_API_URL%|${NX_DATA_API_URL}|g" "$file"
+  sed -i \
+    -e "s|%NODE_ENV%|${NODE_ENV}|g" \
+    -e "s|%NX_CDN_ASSETS%|${NX_CDN_ASSETS}|g" \
+    -e "s|%NX_ENABLE_TELEMETRY%|${NX_ENABLE_TELEMETRY}|g" \
+    -e "s|%NX_URL_PREFIX%|${NX_URL_PREFIX}|g" \
+    -e "s|%NX_DATA_API_URL%|${NX_DATA_API_URL}|g" \
+    -e "s|%NX_SERVER_VERSION%|${NX_SERVER_VERSION}|g" \
+    -e "s|%NX_CONSOLE_MODE%|${NX_CONSOLE_MODE}|g" \
+    -e "s|%NX_HASURA_CONSOLE_TYPE%|${NX_HASURA_CONSOLE_TYPE}|g" \
+    "$file"
+
 done
 
 echo "Starting Nginx..."
